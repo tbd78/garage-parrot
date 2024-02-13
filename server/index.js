bb321167efbf3625fc155e3fdf5688063e09baaa
momo63 <mtambadou@outlook.com>
2024-02-13 02:05:02 +0100

    route mis à jour pour plus de clarté

--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // import routers
-const feedbackRouter = require("./routes/api/feedback");
-const userRouter = require("./routes/api/user");
-const serviceRouter = require("./routes/api/service");
+const feedbackRoute = require("./routes/api/feedback");
+const userRoute = require("./routes/api/user");
+const serviceRoute = require("./routes/api/service");
+const carRoute = require("./routes/api/car");
 
 const app = express();
 const port = process.env.WEB_PORT;
@@ -16,7 +17,11 @@ const port = process.env.WEB_PORT;
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use("/api", [feedbackRouter, userRouter, serviceRouter]);
+app.use("/api/feedback", feedbackRoute);
+app.use("/api/user", userRoute);
+app.use("/api/service", serviceRoute);
+app.use("/api/car", carRoute);
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
