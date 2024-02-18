9b42eefc9a1cc15b97ab3715f899a7135d0a8df2
momo63 <mtambadou@outlook.com>
2024-02-18 05:04:34 +0100

    Ajout de schemas de validation 'car'

--- server/routes/api/car.js
+++ server/routes/api/car.js
@@ -10,9 +10,36 @@ const { DeleteCar } = require("../../query/delete");
 // import les controlleurs génériques
 const controller = require("./router_controller");
 
+// import de 'zod' pour la validation
+const { z } = require("zod");
+
+// schema de validation à l'insertion
+const INSERT_SCHEMA_VALIDATOR = z.object({
+    brand: z.string().min(2).max(50),
+    model: z.string().min(1).max(50),
+    price: z.number().int(),
+    mileage: z.number().int(),
+    year: z.number().int(),
+    cover_image: z.string().min(1).max(100),
+    sold: z.boolean()
+});
+
+// schema de validation à la mise à jour
+const UPDATE_SCHEMA_VALIDATOR = z.object({
+    id: z.number().int(),
+    car_id: z.number().int(),
+    brand: z.string().min(2).max(50),
+    model: z.string().min(1).max(50),
+    price: z.number().int(),
+    mileage: z.number().int(),
+    year: z.number().int(),
+    cover_image: z.string().min(1).max(100),
+    sold: z.boolean()
+});
+
 // insère une voiture
 router.post('/', async (req, res) => {
-    controller.Insert(req, res, InsertCar);
+    controller.Insert(req, res, InsertCar, INSERT_SCHEMA_VALIDATOR);
 });
 
 // récupère toutes les voitures
@@ -27,7 +54,7 @@ router.get('/:id', async (req, res) => {
 
 // modifie une voiture
 router.put('/', async (req, res) => {
-    controller.Update(req, res, UpdateCar);
+    controller.Update(req, res, UpdateCar, UPDATE_SCHEMA_VALIDATOR);
 });
 
 // supprimer une voiture
