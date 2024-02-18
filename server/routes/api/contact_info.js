34d4249c5651a7b7f176b6cd265ba5f9f58cb122
momo63 <mtambadou@outlook.com>
2024-02-18 05:07:54 +0100

    Ajout de schemas de validation 'contact_info'

--- server/routes/api/contact_info.js
+++ server/routes/api/contact_info.js
@@ -10,9 +10,29 @@ const { DeleteContactInfo } = require("../../query/delete");
 // import les controlleurs génériques
 const controller = require("./router_controller");
 
+// import de 'zod' pour la validation
+const { z } = require("zod");
+
+// schema de validation à l'insertion
+const INSERT_SCHEMA_VALIDATOR = z.object({
+    name: z.string().min(1).max(50),
+    address: z.string().min(1).max(50),
+    phone: z.string().min(1).max(10),
+    email: z.string().min(1).max(50)
+});
+
+// schema de validation à la mise à jour
+const UPDATE_SCHEMA_VALIDATOR = z.object({
+    id: z.number().int(),
+    name: z.string().min(1).max(50),
+    address: z.string().min(1).max(50),
+    phone: z.string().min(1).max(10),
+    email: z.string().min(1).max(50)
+});
+
 // insère les infos de contact
 router.post('/', async (req, res) => {
-    controller.Insert(req, res, InsertContactInfo);
+    controller.Insert(req, res, InsertContactInfo, INSERT_SCHEMA_VALIDATOR);
 });
 
 // récupère tous les infos de contact
@@ -27,7 +47,7 @@ router.get('/:id', async (req, res) => {
 
 // modifie un info de contact
 router.put('/', async (req, res) => {
-    controller.Update(req, res, UpdateContactInfo)
+    controller.Update(req, res, UpdateContactInfo, UPDATE_SCHEMA_VALIDATOR)
 });
 
 // supprime les infos de contact
