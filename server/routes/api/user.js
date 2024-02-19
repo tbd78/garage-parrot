b684654feaaa1d4b096e6ff56fa8f1257e1908ac
momo63 <mtambadou@outlook.com>
2024-02-19 13:38:35 +0100

    ajout de schema de validations à 'user'

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -10,9 +10,31 @@ const { DeleteUser } = require("../../query/delete");
 // import les controlleurs génériques
 const controller = require("./router_controller");
 
+// import de 'zod' pour la validation
+const { z } = require("zod");
+
+// schema de validation à l'insertion
+const INSERT_SCHEMA_VALIDATOR = z.object({
+    username: z.string().max(50).email(),
+    password: z.string().min(8),
+    role: z.string().min(4).max(10),
+    firstname: z.string().max(100),
+    lastname: z.string().max(100)
+});
+
+// schema de validation à la mise à jour
+const UPDATE_SCHEMA_VALIDATOR = z.object({
+    id: z.number().int(),
+    username: z.string().max(50).email(),
+    password: z.string().min(8),
+    role: z.string().min(4).max(10),
+    firstname: z.string().max(100),
+    lastname: z.string().max(100)
+});
+
 // insère un utilisateur
 router.post('/', async (req, res) => {
-    controller.Insert(req, res, InsertUser);
+    controller.Insert(req, res, InsertUser, INSERT_SCHEMA_VALIDATOR);
 });
 
 // récupère tous les utilisateurs
@@ -27,7 +49,7 @@ router.get('/:id', async (req, res) => {
 
 // modifie un utilisateur
 router.put('/', async (req, res) => {
-    controller.Update(req, res, UpdateUser);
+    controller.Update(req, res, UpdateUser, UPDATE_SCHEMA_VALIDATOR);
 });
 
 // supprime un utilisateur
