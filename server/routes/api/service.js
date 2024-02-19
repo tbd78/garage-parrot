da575fdb69ae2a8722ea11cabe393de8c3cc14d4
momo63 <mtambadou@outlook.com>
2024-02-19 13:04:32 +0100

    ajout de schemas de validation à 'service'

--- server/routes/api/service.js
+++ server/routes/api/service.js
@@ -10,9 +10,25 @@ const { UpdateService } = require("../../query/update");
 // import les controlleurs génériques
 const controller = require("./router_controller");
 
+// import de 'zod' pour la validation
+const { z } = require("zod");
+
+// schema de validation à l'insertion
+const INSERT_SCHEMA_VALIDATOR = z.object({
+    service_name: z.string().min(1).max(50),
+    description: z.string().max(50)
+});
+
+// schema de validation à la mise à jour
+const UPDATE_SCHEMA_VALIDATOR = z.object({
+    id: z.number().int(),
+    service_name: z.string().min(1).max(50),
+    description: z.string().max(50)
+});
+
 // insère un service
 router.post('/', async (req, res) => {
-    controller.Insert(req, res, InsertService);
+    controller.Insert(req, res, InsertService, INSERT_SCHEMA_VALIDATOR);
 });
 
 // récupère tous les services
@@ -27,7 +43,7 @@ router.get('/:id', async (req, res) => {
 
 // modifie un service
 router.put('/', async (req, res) => {
-    controller.Update(req, res, UpdateService);
+    controller.Update(req, res, UpdateService, UPDATE_SCHEMA_VALIDATOR);
 });
 
 // supprime un service
