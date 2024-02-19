b3d771876df73ab6def502244b24d9d239a36e7b
momo63 <mtambadou@outlook.com>
2024-02-19 13:09:52 +0100

    ajout de schemas de validations à 'spec'

--- server/routes/api/spec.js
+++ server/routes/api/spec.js
@@ -10,9 +10,25 @@ const { DeleteSpec } = require("../../query/delete");
 // import les controlleurs génériques
 const controller = require("./router_controller");
 
+// import de 'zod' pour la validation
+const { z } = require("zod");
+
+// schema de validation à l'insertion
+const INSERT_SCHEMA_VALIDATOR = z.object({
+    name: z.string().min(1).max(50),
+    type: z.string().min(1).max(50)
+});
+
+// schema de validation à la mise à jour
+const UPDATE_SCHEMA_VALIDATOR = z.object({
+    id: z.number().int(),
+    name: z.string().min(1).max(50),
+    type: z.string().min(1).max(50)
+});
+
 // insérer une caractéristique
 router.post('/', async (req, res) => {
-    controller.Insert(req, res, InsertSpec);
+    controller.Insert(req, res, InsertSpec, INSERT_SCHEMA_VALIDATOR);
 });
 
 // récupère toutes les caractéristiques
@@ -27,7 +43,7 @@ router.get('/:id', async (req, res) => {
 
 // modifie une caractéristique
 router.put('/', async (req, res) => {
-    controller.Update(req, res, UpdateSpec)
+    controller.Update(req, res, UpdateSpec, UPDATE_SCHEMA_VALIDATOR);
 });
 
 // supprime une caractéristique
