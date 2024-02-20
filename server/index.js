a018ba7cb642d646cef13b4c10190a28c9054283
momo63 <mtambadou@outlook.com>
2024-02-20 11:37:12 +0100

    routes updated in order to server web resources

--- server/index.js
+++ server/index.js
@@ -13,9 +13,14 @@ const port = process.env.WEB_PORT;
 // for parsing application/json
 app.use(express.json());
 // for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
-// handle api requests
+// routes
+// serve application files
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
 app.use("/api/feedback", require("./routes/api/feedback"));
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/service", require("./routes/api/service"));
@@ -25,8 +30,19 @@ app.use("/api/contact_info", require("./routes/api/contact_info"));
 app.use("/api/car_spec", require("./routes/api/car_specs"));
 app.use("/api/gallery", require("./routes/api/gallery"));
 
-app.use("/api/*", require("./routes/api/router_controller").NotFound)
+app.use("/api/*", require("./routes/api/router_controller").NotFound);
+
+app.all("*", (req, res) => {
+    res.status(404);
+    if(req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "public", "404.html"));
+    } else if(req.accepts("json")) {
+        res.json({ "erreur": "404 ressource introuvable" });
+    } else {
+        res.type("txt").send("404 ressource introuvable");
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 });
